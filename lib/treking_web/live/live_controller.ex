c0cd6a59cdafe9f6cec62260d87cdcc6fe3de97d
zvlasic <zlayo1@gmail.com>
2023-12-12 10:02:47 +0100

    Allow uploads

--- lib/treking_web/live/live_controller.ex
+++ lib/treking_web/live/live_controller.ex
@@ -2,18 +2,37 @@ defmodule TrekingWeb.LiveController do
   use TrekingWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :message, "Hello World")}
+    socket = allow_upload(socket, :results, accept: ~w(.xls .xlsx), max_entries: 1)
+    {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h1><%= @message %></h1>
-        </div>
-      </div>
+    <div id="result-upload">
+      <form id="upload-form" phx-submit="save" phx-change="validate">
+        <.live_file_input upload={@uploads.results} />
+        <.button phx-disable-with="Uploading...">Upload</.button>
+      </form>
     </div>
     """
   end
+
+  def handle_event("save", _params, socket) do
+    consume_uploaded_entries(socket, :results, fn %{path: path}, entry ->
+      dest =
+        Path.join([
+          :code.priv_dir(:treking),
+          "static",
+          "uploads",
+          "#{entry.uuid}-#{entry.client_name}"
+        ])
+
+      File.cp!(path, dest)
+      {:ok, static_path(socket, "/uploads/#{Path.basename(dest)}")}
+    end)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("validate", _params, socket), do: {:noreply, socket}
 end
