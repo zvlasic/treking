b48e1a4b6e4969ec13c4d6452607d6ff5d9411f1
zvlasic <zlayo1@gmail.com>
2023-12-22 21:48:09 +0100

    Add basic total point calcs

--- lib/treking.ex
+++ lib/treking.ex
@@ -1,62 +1,91 @@
 defmodule Treking do
   import Ecto.Query
   alias Treking.Repo
-  alias Treking.Schemas.Race
+  alias Treking.Schemas.{Race, Result, Runner}
+
+  @valid_races 8
+
+  def calculate_points(category, gender) do
+    category =
+      case category do
+        :challenger -> [:challenger, :ultra, :marathon]
+        :active -> [:active]
+      end
+
+    runners =
+      Repo.all(
+        from runner in Runner,
+          where: runner.gender == ^gender,
+          preload: [results: ^from(result in Result, where: result.category in ^category)]
+      )
+
+    Enum.map(runners, fn %{results: results} = runner ->
+      results = results |> Enum.sort_by(& &1.points, :desc) |> Enum.take(@valid_races)
+      total_points = Enum.reduce(results, 0, fn result, acc -> acc + result.points end)
+      per_race = results |> Enum.map(&{&1.race_id, &1.points}) |> Map.new()
+
+      %{runner: runner, total_points: total_points, per_race: per_race}
+    end)
+  end
 
   def get_races, do: Repo.all(from race in Race, order_by: race.date)
 
-  def points do
-    %{
-      1 => 100,
-      2 => 85,
-      3 => 75,
-      4 => 70,
-      5 => 65,
-      6 => 62,
-      7 => 59,
-      8 => 56,
-      9 => 53,
-      10 => 50,
-      11 => 48,
-      12 => 46,
-      13 => 44,
-      14 => 42,
-      15 => 40,
-      16 => 39,
-      17 => 38,
-      18 => 37,
-      19 => 36,
-      20 => 35,
-      21 => 34,
-      22 => 33,
-      23 => 32,
-      24 => 31,
-      25 => 30,
-      26 => 29,
-      27 => 28,
-      28 => 27,
-      29 => 26,
-      30 => 25,
-      31 => 24,
-      32 => 23,
-      33 => 22,
-      34 => 21,
-      35 => 20,
-      36 => 19,
-      37 => 18,
-      38 => 17,
-      39 => 16,
-      40 => 15,
-      41 => 14,
-      42 => 13,
-      43 => 12,
-      44 => 11,
-      45 => 10,
-      46 => 9,
-      47 => 8,
-      48 => 7,
-      49 => 6,
-      50 => 5
-    }
+  def points(position) do
+    Map.get(
+      %{
+        1 => 100,
+        2 => 85,
+        3 => 75,
+        4 => 70,
+        5 => 65,
+        6 => 62,
+        7 => 59,
+        8 => 56,
+        9 => 53,
+        10 => 50,
+        11 => 48,
+        12 => 46,
+        13 => 44,
+        14 => 42,
+        15 => 40,
+        16 => 39,
+        17 => 38,
+        18 => 37,
+        19 => 36,
+        20 => 35,
+        21 => 34,
+        22 => 33,
+        23 => 32,
+        24 => 31,
+        25 => 30,
+        26 => 29,
+        27 => 28,
+        28 => 27,
+        29 => 26,
+        30 => 25,
+        31 => 24,
+        32 => 23,
+        33 => 22,
+        34 => 21,
+        35 => 20,
+        36 => 19,
+        37 => 18,
+        38 => 17,
+        39 => 16,
+        40 => 15,
+        41 => 14,
+        42 => 13,
+        43 => 12,
+        44 => 11,
+        45 => 10,
+        46 => 9,
+        47 => 8,
+        48 => 7,
+        49 => 6,
+        50 => 5,
+        nil => 1
+      },
+      position
+    )
   end
 end
---
+++ test/treking/treking_test.exs
@@ -0,0 +1,82 @@
+defmodule Treking.TrekingTest do
+  use Treking.DataCase, async: true
+
+  alias Treking.Repo
+  alias Treking.Schemas.{Race, Result, Runner}
+
+  setup do
+    races =
+      for i <- 1..12 do
+        Repo.insert!(%Race{name: "#{i}", date: Date.add(~D[2018-01-01], i)})
+      end
+
+    runner =
+      Repo.insert!(%Runner{
+        first_name: "male",
+        last_name: "male surname",
+        birth_year: 1980,
+        gender: :m
+      })
+
+    {:ok, %{races: races, runner: runner}}
+  end
+
+  describe "point calculator" do
+    test "works", %{runner: runner, races: races} do
+      [race1, race2, race3 | _] = races
+      insert_result(runner, race1, 10, :challenger)
+      insert_result(runner, race2, 20, :challenger)
+
+      [result] = Treking.calculate_points(:challenger, :m)
+
+      assert result.runner.id == runner.id
+      assert result.total_points == 85
+      assert Map.get(result.per_race, race1.id) == 50
+      assert Map.get(result.per_race, race2.id) == 35
+      refute Map.get(result.per_race, race3.id)
+    end
+
+    test "takes only 8 races into account", %{races: races, runner: runner} do
+      [race1, race2, race3, race4, race5, race6, race7, race8, race9 | _] = races
+      insert_result(runner, race1, 1, :challenger)
+      insert_result(runner, race2, 1, :challenger)
+      insert_result(runner, race3, 1, :challenger)
+      insert_result(runner, race4, 1, :challenger)
+      insert_result(runner, race5, 1, :challenger)
+      insert_result(runner, race6, 1, :challenger)
+      insert_result(runner, race7, 1, :challenger)
+      insert_result(runner, race8, 1, :challenger)
+      insert_result(runner, race9, 10, :challenger)
+
+      [result] = Treking.calculate_points(:challenger, :m)
+
+      assert result.runner.id == runner.id
+      assert result.total_points == 800
+      assert Map.get(result.per_race, race1.id) == 100
+      assert Map.get(result.per_race, race2.id) == 100
+      assert Map.get(result.per_race, race3.id) == 100
+      assert Map.get(result.per_race, race4.id) == 100
+      assert Map.get(result.per_race, race5.id) == 100
+      assert Map.get(result.per_race, race6.id) == 100
+      assert Map.get(result.per_race, race7.id) == 100
+      assert Map.get(result.per_race, race8.id) == 100
+      refute Map.get(result.per_race, race9.id)
+    end
+
+    test "takes category into account" do
+    end
+
+    test "takes gender into account" do
+    end
+  end
+
+  defp insert_result(runner, race, position, category) do
+    Repo.insert!(%Result{
+      race_id: race.id,
+      runner_id: runner.id,
+      position: position,
+      category: category,
+      points: Treking.points(position)
+    })
+  end
+end
