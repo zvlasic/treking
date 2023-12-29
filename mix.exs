ea95dfec9dcba7708a2e5935bcbeb7888c1799b9
zvlasic <zlayo1@gmail.com>
2023-12-29 09:53:57 +0100

    Update elixir version to latest

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Treking.MixProject do
     [
       app: :treking,
       version: "0.1.0",
-      elixir: "~> 1.15.7",
+      elixir: "~> 1.16.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
