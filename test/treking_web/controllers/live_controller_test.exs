9b31e800b8e5eb825ce20af1a0a0e66e7731be80
zvlasic <zlayo1@gmail.com>
2023-12-30 09:20:05 +0100

    Streamline tests

--- test/treking_web/controllers/live_controller_test.exs
+++ test/treking_web/controllers/live_controller_test.exs
@@ -2,7 +2,7 @@ defmodule TrekingWeb.LiveControllerTest do
   use TrekingWeb.ConnCase, async: true
 
   alias Treking.Repo
-  alias Elixlsx.{Workbook, Sheet}
+  alias Elixlsx.{Sheet, Workbook}
 
   setup do
     on_exit(fn -> path("autogen*") |> Path.wildcard() |> Enum.each(&File.rm!(&1)) end)
@@ -11,29 +11,23 @@ defmodule TrekingWeb.LiveControllerTest do
   describe "uploads" do
     test "don't work without all needed params", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/")
-
       prepare_upload(view, "test1.xlsx")
-
-      assert render_click(view, "upload") =~ "Kornélia"
-
-      assert render_click(view, "insert-results", %{}) =~ "Missing params"
+      assert click_upload(view) =~ "Kornélia"
+      assert click_insert(view, %{}) =~ "Missing params"
     end
 
     test "don't work without selecting file", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/")
-      assert render_click(view, "upload") =~ "Select a file!"
+      assert click_upload(view) =~ "Select a file!"
     end
 
-    test "work", %{conn: conn} do
+    test "work (existing file)", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/")
-
       prepare_upload(view, "test1.xlsx")
+      assert click_upload(view) =~ "Kornélia"
+      race = hd(Repo.all(Treking.Schemas.Race))
 
-      assert render_click(view, "upload") =~ "Kornélia"
-
-      race = Repo.all(Treking.Schemas.Race) |> hd()
-
-      assert render_click(view, "insert-results", %{
+      assert click_insert(view, %{
                "birth_year" => "5",
                "country" => "NO_COUNTRY",
                "fin" => "ALL_FIN",
@@ -46,24 +40,20 @@ defmodule TrekingWeb.LiveControllerTest do
              }) =~ "Inserted 1 results!"
     end
 
-    test "work 2", %{conn: conn} do
-      sheet = %Sheet{
-        name: "Sheet1",
-        rows: [["First Name", "Last Name", "Position"], ["Marko", "Kos", 1], ["Ante", "Gelo", 2]]
-      }
-
-      Workbook.append_sheet(%Workbook{}, sheet)
-      |> Elixlsx.write_to(path("autogen1.xlsx"))
+    test "work (created file)", %{conn: conn} do
+      file_name =
+        create_file([
+          ["First Name", "Last Name", "Position"],
+          ["Marko", "Kos", 1],
+          ["Ante", "Gelo", 2]
+        ])
 
       {:ok, view, _html} = live(conn, ~p"/")
+      prepare_upload(view, file_name)
+      assert click_upload(view) =~ "Marko"
+      race = hd(Repo.all(Treking.Schemas.Race))
 
-      prepare_upload(view, "autogen1.xlsx")
-
-      assert render_click(view, "upload") =~ "Marko"
-
-      race = Repo.all(Treking.Schemas.Race) |> hd()
-
-      assert render_click(view, "insert-results", %{
+      assert click_insert(view, %{
                "birth_year" => "NO_YEAR",
                "country" => "NO_COUNTRY",
                "fin" => "ALL_FIN",
@@ -77,6 +67,17 @@ defmodule TrekingWeb.LiveControllerTest do
     end
   end
 
+  defp create_file(rows) do
+    sheet = %Sheet{name: random_string(), rows: rows}
+    filename = "autogen#{random_string()}.xlsx"
+    path = path(filename)
+    %Workbook{} |> Workbook.append_sheet(sheet) |> Elixlsx.write_to(path)
+    filename
+  end
+
+  defp click_upload(view), do: render_click(view, "upload")
+  defp click_insert(view, params), do: render_click(view, "insert-results", params)
+
   defp prepare_upload(view, filename) do
     view
     |> file_input("#upload-form", :results, [%{name: filename, content: open_file!(filename)}])
@@ -85,4 +86,7 @@ defmodule TrekingWeb.LiveControllerTest do
 
   defp open_file!(file_name), do: file_name |> path() |> File.read!()
   defp path(file_name), do: Path.join([:code.priv_dir(:treking), "results", file_name])
+
+  defp random_string(length \\ 10),
+    do: :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
 end
