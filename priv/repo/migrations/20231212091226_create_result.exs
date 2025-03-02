97d9015cc35fd1ad5a085d79fef5f5362ba4d724
zvlasic <zlayo1@gmail.com>
2025-03-02 16:39:50 +0100

    Fix migration

--- priv/repo/migrations/20231212091226_create_result.exs
+++ priv/repo/migrations/20231212091226_create_result.exs
@@ -8,8 +8,8 @@ defmodule Treking.Repo.Migrations.CreateResult do
     Category.create_type()
 
     create table(:results) do
-      add :runner_id, references(:runners, on_delete: :delete_all, null: false)
-      add :race_id, references(:races, on_delete: :delete_all, null: false)
+      add :runner_id, references(:runners, on_delete: :delete_all), null: false
+      add :race_id, references(:races, on_delete: :delete_all), null: false
       add :position, :integer
       add :category, Category.type()
       add :dnf, :boolean, default: false
