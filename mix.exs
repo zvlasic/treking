10ed9847639c72f206fbf7af7949a0a04284a73c
Zlatko Vlasic <zlayo1@gmail.com>
2025-11-03 09:19:40 +0100

    fix: update Elixir version to 1.19.1 and remove unused environment files

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Treking.MixProject do
     [
       app: :treking,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.19.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
